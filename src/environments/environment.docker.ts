45e03f7c7c3bf0561f77cd6b593f205f87dd12da
Martin Bobbio <martinbobbio1@gmail.com>
2020-04-18 01:08:18 -0300

    Create environment.docker.ts

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  name: 'AdminPro',
+  production: false,
+  backend: 'http://192.168.99.100:8080',
+  googleId: '507173521202-8j409puqm403avu6mkris8fb016s2tvl.apps.googleusercontent.com'
+};
